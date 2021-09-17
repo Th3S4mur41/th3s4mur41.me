93429d919b7d1c9224e05ca37c8d4e02d0e09391
Michaël Vanderheyden <michael.vanderheyden@atos.net>
2021-09-17 12:49:12 +0200

    refactor: replace custom reset through cssremedy

--- webpack.common.js
+++ webpack.common.js
@@ -16,6 +16,10 @@ module.exports = {
 		assetModuleFilename: 'assets/[hash][ext]'
 	},
 	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src/'),
+			'~': path.resolve(__dirname, 'node_modules/')
+		},
 		extensions: ['.js', '.scss']
 	},
 	module: {
