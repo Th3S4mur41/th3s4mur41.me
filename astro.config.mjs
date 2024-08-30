326010bcdd2e3b662ca50ac075e65bd40d1c75f7
Michaël Vanderheyden <hello@th3s4mur41.me>
2024-08-30 14:47:11 +0200

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://th3s4mur41.me",
+	integrations: [sitemap()],
+});
