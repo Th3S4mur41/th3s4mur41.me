02d79c029f4076fce75b9f8f71632267209f3e9b
Michaël Vanderheyden <hello@th3s4mur41.me>
2025-08-27 10:23:18 +0000

    feat: setup initial layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
 	site: "https://th3s4mur41.me",
 	integrations: [sitemap()],
+	devToolbar: {
+		enabled: false, // Disable the Astro toolbar
+	},
 });
