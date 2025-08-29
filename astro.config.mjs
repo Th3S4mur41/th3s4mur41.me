e07a32886652d1d3b7ccd1b63ee3b8a5b97d18c0
Michaël Vanderheyden <hello@th3s4mur41.me>
2025-08-29 11:09:34 +0000

    refactor: convert first two blog posts to mdx for optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://th3s4mur41.me",
-	integrations: [sitemap()],
+	integrations: [sitemap(), mdx()],
 	devToolbar: {
 		enabled: false, // Disable the Astro toolbar
 	},
