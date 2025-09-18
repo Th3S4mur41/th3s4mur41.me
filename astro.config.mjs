042d48ec2dad8852548c6cce18ee6df3716db8a6
Michaël Vanderheyden <hello@th3s4mur41.me>
2025-09-18 08:01:32 +0000

    feat: add prism syntax highlighting for better a11y

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://th3s4mur41.me",
+	markdown: {
+		syntaxHighlight: "prism",
+	},
 	integrations: [sitemap(), mdx()],
 	devToolbar: {
 		enabled: false, // Disable the Astro toolbar
