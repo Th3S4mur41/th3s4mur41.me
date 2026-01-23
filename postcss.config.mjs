d91408ff80c03e76176cd7562720e5d06308317f
Michaël Vanderheyden <hello@th3s4mur41.me>
2026-01-23 17:06:47 +0100

    feat: improve navigation layout and transition (#2045)
    * feat: implement sticky navigation with scroll-state behavior
    
    * chore: update dependencies and refine PostCSS configuration
    
    * fix: adjust syntax for scroll-state container in navigation styles
    
    * feat: enhance navigation layout with logo and responsive styles
    
    * feat: enhance link styles with hover and focus effects
    
    * fix: update box-shadow colors to use oklch for improved contrast
    
    * feat: add view transition name for current navigation item
    
    * fix: minor improvements
    
    * fix: comment out view-transition-name to prevent jumpy link behavior
    
    * fix: adjust logo image formatting and comment out sticky-nav container for future fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,25 +1,32 @@
-export default () => {
-	return {
-		plugins: {
-			"postcss-nesting": {},
-			autoprefixer: {},
-			"@csstools/postcss-oklab-function": {},
-			"postcss-preset-env": {
-				preserve: true,
-				features: {
-					"media-query-ranges": true,
-				},
-			},
-			cssnano: {
-				preset: [
-					"default",
-					{
-						discardComments: {
-							removeAll: true,
-						},
-					},
-				],
+const isProduction = process.env.NODE_ENV === "production";
+
+export default () => ({
+	plugins: {
+		"postcss-preset-env": {
+			stage: 2,
+			autoprefixer: true,
+			preserve: true,
+			features: {
+				"nesting-rules": true,
+				"media-query-ranges": true,
 			},
 		},
-	};
-};
+		...(isProduction
+			? {
+					cssnano: {
+						preset: [
+							"default",
+							{
+								calc: {
+									warnWhenCannotResolve: false, // silence calc division warnings like em/2
+								},
+								discardComments: {
+									removeAll: true,
+								},
+							},
+						],
+					},
+				}
+			: {}),
+	},
+});
