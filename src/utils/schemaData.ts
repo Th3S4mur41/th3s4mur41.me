583f06cbfd44b3e31e0ef5d3c8747cf72d26442b
Michaël Vanderheyden <hello@th3s4mur41.me>
2026-05-18 13:57:06 +0200

    chore: add Credly profile link to person data

--- src/utils/schemaData.ts
+++ src/utils/schemaData.ts
@@ -134,6 +134,7 @@ export const getPersonData = (origin: string) => {
 			"https://techcommunity.microsoft.com/users/th3s4mur41/111608",
 			"https://www.figma.com/@Th3S4mur41",
 			"https://sessionize.com/th3s4mur41/",
+			"https://www.credly.com/users/th3s4mur41/",
 		],
 	};
 };
